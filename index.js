937f1107468c8b2ed1f8f94a1c82ac1f749ce9af
unknown <ajey33@gmail.com>
2023-09-23 03:57:21 +0530

    adding email template

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const express = require("express")
-require("dotenv").config()
+const express = require("express");
+require("dotenv").config();
+const path = require("path");
+const ejs = require("ejs");
 const bodyParser = require('body-parser');
 const rateLimit = require('express-rate-limit');
-const app = express()
+const app = express();
 const cors = require('cors');
+const nodemailer = require("nodemailer")
 
 const MongoClient = require('mongodb').MongoClient;
 const DB_PATH = process.env.MONGO_URI
@@ -19,8 +22,23 @@ app.use(express.json())
 app.use(bodyParser.json());
 app.use(cors());
 app.use(limiter);
+app.use(express.static("public"));
 
-app.get("/",(req,res) => {
+app.set("view engine", "ejs");
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        type: 'OAuth2',
+        user: process.env.MAIL_USERNAME,
+        pass: process.env.MAIL_PASSWORD,
+        clientId: process.env.OAUTH_CLIENTID,
+        clientSecret: process.env.OAUTH_CLIENT_SECRET,
+        refreshToken: process.env.OAUTH_REFRESH_TOKEN
+    }
+});
+
+app.get("/", (req, res) => {
     res.send("Cookzy - Online")
 })
 
@@ -36,10 +54,37 @@ app.post('/enroll', async (req, res) => {
             const collection = db.collection('cookzyMailingList');
             const existingEmail = await collection.findOne({ email: userEmail });
             if (existingEmail) {
-                res.status(401).json({ message: 'Email address already subscribed' });
+                res.status(401).json({ message: 'Email address already joined' });
             } else {
                 await collection.insertOne({ email: userEmail });
-                res.status(200).json({ message: 'Subscription successful' });
+                ejs
+                    .renderFile(path.join(__dirname, 'Assets', 'thank_you.ejs'),
+                        {
+                            survey_link: "https://forms.gle/VsNrEp3wEjJAeyer6"
+                        })
+                    .then(result => {
+                        emailTemplate = result;
+                        let mailOptions = {
+                            from: "cookzy.help@gmail.com",
+                            to: userEmail,
+                            subject: "You're on the List! Let's Cook Up Something Special",
+                            html: emailTemplate
+                        };
+                        transporter.sendMail(mailOptions, function (err, data) {
+                            if (err) {
+                                console.log("Error " + err);
+                            } else {
+                                console.log(`Email sent to ${userEmail} successfully`);
+                            }
+                        });
+                    })
+                    .catch(err => {
+                        res.status(400).json({
+                            message: "Error Rendering emailTemplate",
+                            error: err
+                        });
+                    });
+                res.status(200).json({ message: 'Successfully joined the waitlist' });
             }
         } catch (err) {
             console.log(err)
